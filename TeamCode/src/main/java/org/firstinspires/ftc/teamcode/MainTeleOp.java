e8adcd18bcbcc4e4d74c8eeb2473be2b1bebfacc
CriticalOverload <31810347+CriticalOverload@users.noreply.github.com>
2019-01-20 19:30:33 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import java.util.logging.Level;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -50,7 +51,6 @@ public class MainTeleOp extends LinearOpMode
         
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
-        liftMotor.setDirection(DcMotor.Direction.REVERSE);
         hingeMotor.setDirection(DcMotor.Direction.REVERSE);
         
         //set lift and hinge motors to encoder run-to-position mode
@@ -60,11 +60,17 @@ public class MainTeleOp extends LinearOpMode
         //configure lift variables
         int liftLevel=0;
         int liftTicks=0;
-        int ticksPerLiftCycle=1440;
+        int ticksCycle=1440;
+        
+        //configure bucket position variable
+        double bp = 1.0;
 
         //powerMod variables can reduce robot speed
         double powerMod = 1.0;
         double mechMod = 1.0;
+        
+        //servo parameters
+        bucketServo.scaleRange(.1,1);
         
         telemetry.addData("Hey", "Listen");
         telemetry.addData("Level of lift: ", liftLevel);
@@ -75,7 +81,7 @@ public class MainTeleOp extends LinearOpMode
 
         waitForStart();
         liftMotor.setPower(1);
-        hingeMotor.setPower(0.8);
+        hingeMotor.setPower(0.5);
         while(opModeIsActive())
         {
             /*
@@ -84,22 +90,19 @@ public class MainTeleOp extends LinearOpMode
              */
             if(gamepad1.right_bumper){
                 powerMod = 0.5;
-            }else{
-                powerMod = 1.0;
             }
-            if(gamepad2.right_bumper){
-                mechMod = 0.5;
-                mechMod = 1.0;
+            else{
+                powerMod = 1.0;
             }
             
             //move lift
             if (gamepad1.dpad_left || gamepad1.dpad_right) {
-                liftTicks = (int)(5.75*ticksPerLiftCycle);
+                liftTicks = (int)((5.75*ticksCycle)/2);
                 liftLevel = 1;
                 liftMotor.setTargetPosition(liftTicks);
             }    
             else if (gamepad1.dpad_up) {
-                liftTicks = (int)(6.75*ticksPerLiftCycle);
+                liftTicks = 5800;
                 liftLevel = 2;
                 liftMotor.setTargetPosition(liftTicks);
             } 
@@ -133,6 +136,33 @@ public class MainTeleOp extends LinearOpMode
             }else if(gamepad2.left_bumper){
                 bucketServo.setPosition(0.4);
             }*/
+            
+            //automatically set bucket servo to optimized position
+            if(gamepad2.left_bumper==false){
+                int hp = hingeMotor.getCurrentPosition();
+                if(hp >= 0 && hp < (1.2*ticksCycle)){
+                    //set between 1 and .4
+                    double percent = hp/(1.2*ticksCycle);
+                    bp = 1-(percent*(1-.4));
+                }
+                else if(hp >= (1.2*ticksCycle) && hp <= (3*ticksCycle)){
+                    //set between .4 and .0
+                    double percent = (hp - 1.2*ticksCycle)/(3*ticksCycle-1.2*ticksCycle);
+                    bp = .4-(percent*(.4-.0));
+                }
+
+                bucketServo.setPosition(bp);
+            }
+            
+            //breakage prevention
+            if(hingeMotor.getCurrentPosition() > 3*ticksCycle){
+                hingeMotor.setTargetPosition(hingeMotor.getCurrentPosition());
+            }
+            /*
+            //dump bucket
+            if((gamepad2.dpad_left || gamepad2.dpad_right) && gamepad2.left_bumper){
+                bucketServo.setPosition(0.36);
+            }*/
 
             //intake:spins in intake direction unless override button (x) pressed
             if(gamepad2.x){
@@ -144,48 +174,73 @@ public class MainTeleOp extends LinearOpMode
             
             //manual bucket positioning
             if(gamepad2.dpad_up){
-                bucketServo.setPosition(bucketServo.getPosition()+0.01*powerMod);
-            }else if(gamepad2.dpad_down){
-                bucketServo.setPosition(bucketServo.getPosition()-0.01*powerMod);
+                bucketServo.setPosition(bucketServo.getPosition()+0.01);
+            }
+            else if(gamepad2.dpad_down){
+                bucketServo.setPosition(bucketServo.getPosition()-0.01);
             }
             
             //Arm movement
             if(gamepad2.right_stick_y!=0){
-                hingeMotor.setTargetPosition((int)(hingeMotor.getCurrentPosition()+(gamepad2.right_stick_y*30*powerMod))); 
+                hingeMotor.setTargetPosition((int)(hingeMotor.getCurrentPosition()-(gamepad2.right_stick_y*30*powerMod)));
             }
             else if(gamepad2.a){
                 hingeMotor.setTargetPosition(0);
+                if(touchSensor.getState() == false){
+                    hingeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    hingeMotor.setPower(0);
+                    Thread.sleep(1000);
+                    hingeMotor.setPower(0.5);
+                    Thread.sleep(1000);
+                    hingeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                }
             }
             else if(gamepad2.b){
-                hingeMotor.setTargetPosition((int)(1.2*ticksPerLiftCycle));
+                hingeMotor.setTargetPosition((int)(1.2*ticksCycle));
+            }
+            else if(gamepad2.y){
+                hingeMotor.setTargetPosition(3*ticksCycle);
             }
             if(gamepad2.right_stick_button){
                 hingeMotor.setTargetPosition(hingeMotor.getCurrentPosition());
             }
             
-            //insert after removing all other arm controls to move arm to starting position
+            
+            //DEBUGGING:insert after removing all other arm controls to 
+            //move arm to starting position
             /*if (gamepad2.right_stick_button){
                 hingeMotor.setPower(-1);
             }*/
             
             
             //if the touch sensor is pressed, then stop motors
-            if(touchSensor.getState() == false){
-                telemetry.addData("Touch Sensor","Pressed");
-                //hingeMotor.setPower(0); won't work with encoding
+            /*if(touchSensor.getState() == false){
+                    telemetry.addData("Touch Sensor","Pressed");
+                    if(gamepad2.right_bumper){
+                        hingeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        hingeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        hingeMotor.setTargetPosition(0);
+                    }
+                }*/
+                
+                
                 //NEED TO READD TOUCH SENSOR BY ZEROING ENCODERS hingeMotor
-            }else{
-                telemetry.addData("Touch Sensor","Not pressed");
-            }
+            
+            else{telemetry.addData("Touch Sensor","Not pressed");}
             
             telemetry.addData("Hey", "Listening");
             telemetry.addData("Level of lift: ", liftLevel);
             telemetry.addData("Encoder Position ", liftMotor.getCurrentPosition());
             telemetry.addData("Lift Ticks ", liftTicks);
-            telemetry.addData("Servo position", bucketServo.getPosition());
+            telemetry.addData("Bucket position", bucketServo.getPosition());
             telemetry.addData("Arm Position: ", hingeMotor.getCurrentPosition());
-            telemetry.addData("2lY ", gamepad2.left_stick_y);
+            telemetry.addData("g2rstcky: ", gamepad2.right_stick_y);
             telemetry.update();
+            
+            if(gamepad2.right_bumper){
+                hingeMotor.close();
+                //hingeMotor.setPower(0);
+            }
 
             idle();
         }
